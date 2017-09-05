0458080be385a568abccc236ac103ca84a102baf
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-09-06 00:48:48 +0530

    Create problem 3.cpp

---
+++ problem 3.cpp
@@ -0,0 +1,20 @@
+if (a>=0)
+{#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+cout<<"enter a number:";
+cin>>a;
+cout<<"a is positive";
+}
+else if (a<0)
+{
+cout<<"a is negative";
+}
+else
+{
+cout<<"a is equal to 0";
+}
+	return 0;
+}
