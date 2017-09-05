04f0dc39247a87fb1c7ecb2d303656599783cd12
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-09-06 00:56:31 +0530

    Update problem 3.cpp

--- problem 3.cpp
+++ problem 3.cpp
@@ -1,12 +1,13 @@
 
-{#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
 	int a;
 cout<<"enter a number:";
 cin>>a;
-if (a>=0)	
+if (a>=0)
+{	
 cout<<"a is positive";
 }
 else if (a<0)
